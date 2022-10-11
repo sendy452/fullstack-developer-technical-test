d80b072616ebd9d3d13c132553b67f75e9e90799
sendy452 <sendyivenyulian452@gmail.com>
2022-10-11 18:23:30 +0700

    set model

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Employee extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'position_id',
+        'nama_karyawan',
+        'nip',
+        'departemen',
+        'tgl_lahir',
+        'thn_lahir',
+        'alamat',
+        'no_telp',
+        'agama',
+        'status',
+        'ktp'
+    ];
 }
